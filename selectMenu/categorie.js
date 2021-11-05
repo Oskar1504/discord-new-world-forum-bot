25cab7b28f9abb792ca96c0d5460341a6fd4ca3b
Oskar Pester <oskar.pester2@gmail.com>
2021-11-05 16:46:34 +0100

    added topic selection / create thread and post all posts

--- selectMenu/categorie.js
+++ selectMenu/categorie.js
@@ -1,4 +1,4 @@
-const { MessageEmbed  } = require('discord.js');
+const { MessageActionRow, MessageEmbed, MessageSelectMenu  } = require('discord.js');
 const axios = require("axios")
 
 
@@ -11,29 +11,46 @@ module.exports = {
             .then(async function (response) {
                 console.log(axios_url, response.status);
 
-                let fields = response.data.topic_list.topics
+                let fields = response.data.topic_list.topics.slice(0,24)
+                let selectOptions = fields.map( elm => {
+                    return {
+                        label:elm.title,
+                        description:`Views:${elm.views} | Likes:${elm.like_count} | Replys: ${elm.reply_count}`,
+                        value:`/t/${elm.id}.json`
+                    }
+                })
                 fields = fields.map( elm => {
                     return {
                         name: elm.title,
-                        value:`Views:${elm.views} | Likes:${elm.like_count} | Replys: ${elm.reply_count}\n[Link](${nw_host}/t/${elm.slug})`
+                        value:`Views:${elm.views} | Likes:${elm.like_count} | Replys: ${elm.reply_count} | [Link](${nw_host}/t/${elm.slug})`
                     }
                 })
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageSelectMenu()
+                            .setCustomId('topic')
+                            .setPlaceholder('Select topic for details')
+                            .addOptions(selectOptions)
+                            .setMaxValues(1),
+                    );
 
                 await interaction.reply({
                     embeds: [
                         new MessageEmbed()
-                            .setTitle(`NW Forum categorie  ${interaction.values[0]}`)
+                            .setTitle(`NW Forum categorie ${interaction.values[0]}`)
                             .setURL(axios_url)
                             .setColor("GOLD")
                             .addFields(fields)
                             .setTimestamp(new Date())
-                    ]
+                    ],
+                    components:[row]
                 })
             })
             .catch(async function(error) {
                 console.log(error.toString());
                  await interaction.reply({
-                    content: `Error ${error.status} while requesting ${axios_url}`
+                    content: `Error ${error.status} while requesting ${axios_url}`,
+                     ephemeral:true
                 })
             })
     }
---
+++ selectMenu/topic.js
@@ -0,0 +1,45 @@
+
+const axios = require("axios")
+const { convert } = require('html-to-text');
+
+module.exports = {
+    async execute(interaction) {
+        let nw_host = "https://forums.newworld.com",
+            axios_url = nw_host + interaction.values[0]
+
+        await axios.get(axios_url)
+            .then(async function (response) {
+                console.log(axios_url, response.status);
+                let posts = response.data.post_stream.posts
+                posts = posts.map( elm => {
+                    let c = convertPostToMsg(elm)
+                    return {
+                        content: (c.length < 4000)?c:"post to long"
+                    }
+                })
+                let thread = interaction.channel.threads.cache.find(x => x.name === response.data.title);
+                if(thread){ await thread.delete();}
+
+                thread = await interaction.channel.threads.create({
+                    name: response.data.title,
+                    autoArchiveDuration: 60
+                });
+                for (const post of posts) {
+                    await thread.send(post)
+                }
+                interaction.deferUpdate(true)
+
+            })
+            .catch(async function(error) {
+                console.log(error.toString());
+                 await interaction.reply({
+                    content: `Error ${error.toString()}`,
+                     ephemeral:true
+                })
+            })
+    }
+};
+
+function convertPostToMsg(elm){
+    return `>>>Author:${elm.username}\n\n ${convert(elm.cooked, {wordwrap: 130})}`
+}
